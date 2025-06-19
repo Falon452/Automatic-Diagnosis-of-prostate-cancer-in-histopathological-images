f5c9f4063b0b6ddfe1510a12ef36d0ad5f5f8603
Falon <falon4522@gmail.com>
2025-06-19 14:41:33 +0200

    slurm

---
+++ train20.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=job_name
+#SBATCH --time=48:00:00
+#SBATCH --account=plgmpr25-gpu-a100
+#SBATCH --partition=plgrid-gpu-a100
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=140G
+#SBATCH --gres=gpu
+#SBATCH --output="joblog-%j.txt"
+#SBATCH --error="joberr-%j.txt"
+#SBATCH --gres=gpu:8
+
+module purge
+module load GCC/11.3.0
+module load GCCcore/10.3.0
+module load Python/3.9.5
+module load OpenMPI/4.1.4
+module load PyTorch/1.13.1-CUDA-11.7.0
+
+pip install torchvision
+pip install pandas
+pip install matplotlib
+
+srun python auc.py --output_dir run_20_verylast --test --resume_epoch 93 --root_dir DiagSet-A --magnification 20x --batch_size 1024  --weight_decay 1e-5 --epochs 1000 --freeze_schedule "[[69,3,1e-7]]"
